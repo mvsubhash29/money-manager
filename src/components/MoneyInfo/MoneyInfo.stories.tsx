02a45e5b584228104ff636b0fdbfe004ed650cb2
M V Subhash <mvsubhash29@gmail.com>
2021-03-23 21:10:29 +0530

    Added MoneyInfo Component

---
+++ src/components/MoneyInfo/MoneyInfo.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {Story, Meta} from '@storybook/react/types-6-0';
+
+import {MoneyInfo as MoneyInfoComponent, MoneyInfoProps} from './MoneyInfo';
+
+export default {
+  title: 'Components/MoneyInfo',
+  component: MoneyInfoComponent
+} as Meta;
+
+const Template: Story<MoneyInfoProps> = (args) => (
+  <MoneyInfoComponent {...args} />
+);
+
+export const MoneyInfo = Template.bind({});
+MoneyInfo.args = {
+  amount: 100,
+  isExpense: false,
+  categoryName: 'Food'
+};
---
+++ src/components/MoneyInfo/MoneyInfo.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  Box,
+  createStyles,
+  Icon,
+  makeStyles,
+  Theme,
+  Typography
+} from '@material-ui/core';
+import {green} from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    title: {
+      fontSize: 14
+    },
+    categoryName: {
+      marginLeft: theme.spacing(1)
+    },
+    container: {
+      padding: theme.spacing(1)
+    }
+  })
+);
+
+export interface MoneyInfoProps {
+  categoryName: string;
+  amount: number;
+  isExpense: boolean;
+}
+
+export const MoneyInfo = (props: MoneyInfoProps) => {
+  const {categoryName, amount, isExpense} = props;
+  const classes = useStyles();
+
+  return (
+    <Box
+      className={classes.container}
+      display='flex'
+      alignItems='center'
+      justifyContent='space-between'
+    >
+      <Box display='flex' alignItems='center'>
+        <Icon style={{color: green[500]}}>start</Icon>
+        <Typography
+          className={classes.categoryName}
+          variant='body2'
+          component='p'
+        >
+          {categoryName}
+        </Typography>
+      </Box>
+      <Typography color='textSecondary'>
+        {isExpense ? '-' : ''} {amount}
+      </Typography>
+    </Box>
+  );
+};
